d5fbb99ab2114f2ee7e77791608c3382ad236092
saurabh vakil <saurash.vakil@gmail.com>
2019-05-23 09:54:43 +0000

    Sixth commit

--- webapp/controller/Info.controller.js
+++ webapp/controller/Info.controller.js
@@ -14,8 +14,31 @@ sap.ui.define([
 			this.getRouter().getRoute("Info").attachPatternMatched(this._onInfoMatched, this);
 		},
 
+		/**
+		 * Binds the view to the object path and expands the aggregated line items.
+		 * @function
+		 * @param {sap.ui.base.Event} oEvent pattern match event in route 'object'
+		 * @private
+		 */
 		_onInfoMatched: function (oEvent) {
-			this.getModel("appView").setProperty("/layout", "ThreeColumnsMidExpanded");
+			//this.getModel("appView").setProperty("/layout", "ThreeColumnsMidExpanded");
+			var sObjectId = oEvent.getParameter("arguments").objectId,
+				sItemPosition = oEvent.getParameter("arguments").itemPosition;
+
+			this.getModel("appView").setProperty("/layout", "ThreeColumnsEndExpanded");
+			this.getModel().metadataLoaded().then(function () {
+				var sObjectPath = this.getModel().createKey("SalesOrderLineItemSet", {
+					SalesOrderID: sObjectId,
+					ItemPosition: sItemPosition
+				});
+				this.getView().bindElement({
+					path: "/" + sObjectPath,
+					parameters: {
+						'expand': 'ToHeader'
+					}
+				});
+			}.bind(this));
+
 		}
 
 		/**
